b2d78e854d0bf91da28e9f079a8df217a1df1b65
Ziassy <fauziahfx216@gmail.com>
2024-08-04 21:26:55 +0700

    [CHANGE] Database to postgree

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'anton_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'triokarya_db',
+        'USER': 'postgres',
+        'PASSWORD': 'triokarya12',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -194,12 +194,12 @@ if DEBUG is False:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'db_name',
-            'USER': 'db_user',
-            'PASSWORD': 'db_password',
-            'HOST': 'www.domain-kita.com',
-            'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'triokarya_db',
+        'USER': 'postgres',
+        'PASSWORD': 'triokarya12',
+        'HOST': 'www.domain-kita.com',
+        'PORT': '5432',
         }
     }
 
